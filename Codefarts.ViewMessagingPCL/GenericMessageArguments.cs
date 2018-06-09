c2f66f96da43c27344c79e0e49bf19d1a0a30e7b
Dean Lunz <deanlunz@createdbyx.com>
2018-06-09 13:03:01 -0700

    changed dictionary signature and added build method

--- Codefarts.ViewMessagingPCL/GenericMessageArguments.cs
+++ Codefarts.ViewMessagingPCL/GenericMessageArguments.cs
@@ -1,24 +1,45 @@
 ﻿namespace Codefarts.ViewMessaging
 {
-    using System;
     using System.Collections.Generic;
 
     public class GenericMessageArguments
     {
-        public static IDictionary<string, string> Show
+        public static IDictionary<string, object> Build(params IDictionary<string, object>[] parts)
+        {
+            var args = new Dictionary<string, object>();
+            foreach (var dictionary in parts)
+            {
+                foreach (var pair in dictionary)
+                {
+                    args[pair.Key] = pair.Value;
+                }
+            }
+
+            return args;
+        }
+
+
+        public static IDictionary<string, object> Show
         {
             get
             {
-                var arguments = new Dictionary<string, string>();
+                var arguments = new Dictionary<string, object>();
                 arguments["show"] = string.Empty;
                 return arguments;
             }
         }
 
-        public static IDictionary<string, string> ShowDialog(string viewId)
+        public static IDictionary<string, object> ShowDialog(string viewId)
         {
-            var arguments = new Dictionary<string, string>();
+            var arguments = new Dictionary<string, object>();
             arguments["showdialog"] = viewId;
+            return arguments;
+        }
+
+        public static IDictionary<string, object> SetModel(object model)
+        {
+            var arguments = new Dictionary<string, object>();
+            arguments["setmodel"] = model;
             return arguments;
         }
     }
