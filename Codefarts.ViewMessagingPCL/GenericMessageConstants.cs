1833c714ef09e6853450a08b19356000fffe8dc0
Dean Lunz <deanlunz@createdbyx.com>
2020-02-23 11:16:21 -0800

    Replaces strings with constants WpfViewService caching should not work in multi threaded environments added AppendedName property to WpfViewService with default value of "View" allowing user to specify appended name if desired misc changes

---
+++ Codefarts.ViewMessagingPCL/GenericMessageConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Codefarts.ViewMessaging
+{
+    public class GenericMessageConstants
+    {
+        public const string Show = "Show";
+        public const string SetModel = "SetModel";
+        public const string ShowDialog = "ShowDialog";
+    }
+}
