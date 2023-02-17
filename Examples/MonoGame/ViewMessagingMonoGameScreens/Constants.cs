249c40f3add016745bd9186a2ff04996c2656d67
Dean Lunz <deanlunz@createdbyx.com>
2023-02-17 03:20:47 -0800

    content and constants

---
+++ Examples/MonoGame/ViewMessagingMonoGameScreens/Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace ViewMessagingMonoGameScreens;
+
+public class Constants
+{
+    public const string MoveUp = nameof(MoveUp);
+    public const string MoveDown = nameof(MoveDown);
+    public const string MoveLeft = nameof(MoveLeft);
+    public const string MoveRight = nameof(MoveRight);
+    public const string MoveHorizontialy= nameof(MoveHorizontialy);
+    public const string MoveVerticaly = nameof(MoveVerticaly);
+    public const string Pause = nameof(Pause);
+
+    public const string MenuExit = nameof(MenuExit);
+    public const string MenuSelect = nameof(MenuSelect);
+    public const string MenuUp = nameof(MenuUp);
+    public const string MenuDown = nameof(MenuDown);
+}
